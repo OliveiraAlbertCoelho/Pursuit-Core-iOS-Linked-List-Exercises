7a74c295ab866bc562e150ebf6fd19224c1a26c2
OliveiraAlbertCoelho <albertoliveira@pursuit.org>
2019-08-28 21:55:31 -0400

    some changes

--- LinkedListExercises/LinkedList.swift
+++ LinkedListExercises/LinkedList.swift
@@ -18,29 +18,54 @@ public class Node<T> {
 
 public class LinkedList<T: Equatable> {
     var head: Node<T>?
+      
     func printAllKeys() {
         var currentNode = head
         while currentNode != nil {
             print(currentNode!.key)
             currentNode = currentNode?.next
+       
         }
     }
-
-    var count: Int {return 0}
+    var count: Int {
+        var count = 0
+        var currentNode = head
+        while currentNode != nil {
+            currentNode = currentNode?.next
+            count += 1
+        }
+        return count
+    }
     
-    func append(element newKey: T) {}
+    func append(element newKey: T){
+        if head == nil {
+            head = Node(key: newKey)
+            return
+        }
+        var currentNode = head
+        while currentNode?.next != nil {
+            currentNode = currentNode?.next
+        }
+        currentNode?.next = Node(key: newKey)
+    }
+    func getNode(at index: Int) -> Node<T>? {return nil}
     
-    func getNode<T>(at index: Int) -> Node<T>? {return nil}
+    func contains(element targetKey: T) -> Bool {
+        var currentNode = head
+        while currentNode != nil {
+            if currentNode?.key == targetKey { return true }
+            currentNode = currentNode?.next
+        }
+        return false
+    }
+    func equals(otherList: LinkedList<T>) -> Bool {return true}
     
-    func contains<T>(element targetKey: T) -> Bool {return false}
+    func toArr() -> [T] {return Array<T>()}
     
-    func equals<T>(otherList: LinkedList<T>) -> Bool {return true}
-    
-    func toArr<T>() -> [T] {return Array<T>()}
-    
-    func reversed<T>() -> LinkedList<T> {return LinkedList<T>()}
+    func reversed() -> LinkedList<T> {return LinkedList<T>()}
     
     func removeAll() {}
+    
     
     //Challenge Questions
     func removeDuplicatesFromSortedList() {}
